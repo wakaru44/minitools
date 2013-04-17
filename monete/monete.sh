fddf44de7c275c8b88479e6ae311a03586986660
Wakaru Himura <wakaru44@gmail.com>
2013-04-18 01:02:26 +0200

    documented a bit

--- monete/monete.sh
+++ monete/monete.sh
@@ -8,15 +8,31 @@
 # 3.- detecta si hay diferencias
 # 4.- envia el email alertando (a. si es distinta de la salida anterior)
 
+##############################
+#	Configuration
+##############################
 
-# Command to launch
+# Which command to launch?
+# 	Try to get an output that will distinguish betwen
+#	one situation that you want to be alerted for and
+#	when you not
 COMMAND="ls -l"
 
-# where is the working dir
+# where is the working dir located?
+#	where the script will put all its working files and stamps.
+# 	remember to give write permissions ;)
 WORK_DIR=/home/wakaru/src/github/minitools/monete/tmp/foo
 
 # who wants to know about this?
+#	Email address to notify. 
+#	Be sure to have enabled outgoing mail
+# 	and mailutils installed
 EMAIL="wakaru44@gmail.com"
+
+
+##############################
+#	The inner magic
+##############################
 
 function launch 
 {
@@ -40,13 +56,38 @@ function emailit
 
 }
 
+function prepare_environment
+{
+
+	if [ -e $WORK_DIR ]
+	then
+		echo "directory exists"
+	else
+		echo "directory nonexistant"
+		echo "Creating work directory:  $WORK_DIR"
+		#TODO: ask for confirmation
+		mkdir -p $WORK_DIR
+	fi
+
+}
+
+
+##############################
+#	Actual Script ;)
+##############################
+
+# check the working dir and so
+prepare_environment
+# launch a comand
 launch
+# test the output
 if [ differ ]
 then
 	echo "difiere"
 else
 	echo "iguales"
 fi
+# and notify
 emailit
 
 
