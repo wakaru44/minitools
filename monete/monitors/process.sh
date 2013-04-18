363c078cf236565c15d703493161ad24173538d3
Wakaru Himura <wakaru44@gmail.com>
2013-04-19 00:56:59 +0200

    enabled something

--- monete/monitors/process.sh
+++ monete/monitors/process.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
-pgrep $1 >/dev/null
-if [ $? -eq 0 ]
-then
-	echo "$1 OK"
-else
-	echo "$1 Jodido"
 
+USAGE="This little script checks if a process is running.
+	Usage:
+		
+		$0 processname
+
+	It works pretty much like pgrep ;)"
+
+if [ $1 ]
+then
+	#check that the process is running 
+	pgrep $1 >/dev/null
+	if [ $? -eq 0 ]
+	then
+		echo "$1 OK"
+	else
+		echo "$1 Jodido"
+	fi
+else
+	# show usage and exit with error
+	echo "$USAGE"
+	exit 1
 fi
+
