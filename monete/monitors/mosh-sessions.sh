82b61ad522996fea57cb38c221abda3f67d80c72
wakaru44 <wakaru44@gmail.com>
2013-08-28 15:16:01 +0200

    With Antxon
    Simplified Version Just with awk

--- monete/monitors/mosh-sessions.sh
+++ monete/monitors/mosh-sessions.sh
@@ -2,5 +2,6 @@
 # Display the user and port of all the mosh sessions opened in this server
 # Must be run as root, or it will only find out the runing user sessions
 
-ps -ef | grep -v grep  | grep -v $0 | grep mosh | awk '{ print "Esta " $1 " En el puerto " $12 }'
+PS=$(ps -ef)
+echo "$PS" | awk '/mosh/ && !/'$0'/ { print "Esta " $1 " En el puerto " $12 }'
 
